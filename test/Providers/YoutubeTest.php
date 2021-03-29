6251cc529d07eeff49495d1954910de477f79e7c
simba77 <simba7@inbox.ru>
2021-03-30 00:12:22 +0300

    Added test for the youtube provider

---
+++ test/Providers/YoutubeTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Simba77\EmbedMediaTest\Providers;
+
+use PHPUnit\Framework\TestCase;
+use Simba77\EmbedMedia\Providers\Youtube;
+
+class YoutubeTest extends TestCase
+{
+    /**
+     * @var string
+     */
+    protected $html_string = '';
+
+    protected function setUp(): void
+    {
+        $this->html_string = '<figure class="media"><oembed url="https://youtu.be/8ZLSKEmbt0Y?t=75"></oembed></figure><figure class="media"><oembed url="https://youtu.be/8ZLSKEmbt0Y"></oembed></figure>';
+    }
+
+    public function testParse(): void
+    {
+        $youtube = new Youtube(['player_classes' => 'player_classes']);
+        $content = $youtube->parse($this->html_string);
+        $this->assertEquals(
+            '<figure class="media"><div style="max-width: 600px"><div class="player_classes">' .
+            '<iframe allowfullscreen="allowfullscreen" src="//www.youtube.com/embed/8ZLSKEmbt0Y?start=75"></iframe></div></div></figure><figure class="media"><div style="max-width: 600px"><div class="player_classes">' .
+            '<iframe allowfullscreen="allowfullscreen" src="//www.youtube.com/embed/8ZLSKEmbt0Y"></iframe></div></div></figure>',
+            $content
+        );
+
+        $youtube = new Youtube(['player_max_width' => '900px']);
+        $content = $youtube->parse($this->html_string);
+        $this->assertEquals(
+            '<figure class="media"><div style="max-width: 900px"><div class="">' .
+            '<iframe allowfullscreen="allowfullscreen" src="//www.youtube.com/embed/8ZLSKEmbt0Y?start=75"></iframe></div></div></figure><figure class="media"><div style="max-width: 900px"><div class="">' .
+            '<iframe allowfullscreen="allowfullscreen" src="//www.youtube.com/embed/8ZLSKEmbt0Y"></iframe></div></div></figure>',
+            $content
+        );
+    }
+}
