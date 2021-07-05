89a67f6e5d9f7bb266f4e96d798246f2abe80c5e
simba77 <simba7@inbox.ru>
2021-07-05 17:51:22 +0300

    Bug fixes

--- src/Providers/Youtube.php
+++ src/Providers/Youtube.php
@@ -57,7 +57,8 @@ class Youtube implements EmbedProvider
         } catch (InvalidSelectorException $e) {
         }
 
-        return $document->html();
+        // We replace it because the standard LIBXML_HTML_NOIMPLIED flag leads to unexpected behavior when working with nodes.
+        return str_replace(["<html><body>\n", "\n</body></html>"], '', $document->html());
     }
 
     protected function getStyles(): string
